0edb2d2da336d360dfc415dddc63d89c17636a3a
Lucia Mezquida <lucia.mezquida@gmail.com>
2021-03-05 20:11:41 +0100

    Render operator buttons in Calculator

--- src/components/Calculator/Calculator.tsx
+++ src/components/Calculator/Calculator.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 
 const rows = [[7, 8, 9], [4, 5, 6], [1, 2, 3], [0]];
+const operators = ['+', '-', '×', '÷'];
+
 const Calculator = () => {
   const renderRows = rows.map(row => {
     return(
@@ -13,11 +15,15 @@ const Calculator = () => {
     )
   }
   )
+  const renderOperators = operators.map(op => (
+    <button key={op}>{op}</button>
+  ))
   return(
     <>
     <h1>Calculator</h1>
     <div className="calculator" role='grid'>
       {renderRows}
+      {renderOperators}
     </div>
     </>
     

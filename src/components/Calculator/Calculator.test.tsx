77207e69bab2ac733455214b95e9a0f36e84ca06
Lucia Mezquida <lucia.mezquida@gmail.com>
2021-03-05 19:30:20 +0100

    Add test to render number buttons

---
+++ src/components/Calculator/Calculator.test.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Calculator from './Calculator';
+
+describe('<Calculator />', () => {
+  it('shows numbers', ()=> {
+    render(<Calculator />);
+    const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    numbers.forEach(number => {
+      expect(screen.getByText(number.toString())).toBeInTheDocument();
+    })
+  })
+})
--- src/components/Calculator/Calculator.tsx
+++ src/components/Calculator/Calculator.tsx
@@ -1,7 +1,14 @@
 import React, { useState } from 'react'
+
+
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 const Calculator = () => {
   return(
+    <>
     <h1>Calculator</h1>
+    
+    </>
+    
   )
 }
 

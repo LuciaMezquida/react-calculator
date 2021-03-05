927b5cebe9b5873ec3bb89bceac546ff4d862ef7
Lucia Mezquida <lucia.mezquida@gmail.com>
2021-03-05 18:56:38 +0100

    Create Calculator component

---
+++ src/components/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Calculator from './Calculator/Calculator';
+
+function App() {
+  return (
+    <div className="App">
+      <main>
+        <Calculator />
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Calculator/Calculator.tsx
@@ -0,0 +1,8 @@
+import React, { useState } from 'react'
+const Calculator = () => {
+  return(
+    <h1>Calculator</h1>
+  )
+}
+
+export default Calculator

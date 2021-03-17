5b6f8e7c4c1c1d91fedb7e422059969b6b4f3c5b
Lucia Mezquida <lucia.mezquida@gmail.com>
2021-03-17 16:47:49 +0100

    Test the displays of the user's inputs

--- src/components/Calculator/Calculator.test.tsx
+++ src/components/Calculator/Calculator.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import Calculator from './Calculator';
 
 describe('<Calculator />', () => {
@@ -39,5 +39,40 @@ describe('<Calculator />', () => {
   it("renders an input disabled", () => {
     render(<Calculator />);
     expect(screen.getByPlaceholderText("calculate")).toBeDisabled();
+  });
+  it("displays users inputs", async () => {
+    render(<Calculator />);
+    const one = screen.getByText("1");
+    const two = screen.getByText("2");
+    const plus = screen.getByText("+");
+    fireEvent.click(one);
+    fireEvent.click(plus);
+    fireEvent.click(two);
+
+    const result = await screen.findByPlaceholderText("calculate");
+    // @ts-ignore
+    expect(result.value).toBe("1+2");
+  });
+
+  it("displays multiple users inputs", async () => {
+    render(<Calculator />);
+    const one = screen.getByText("1");
+    const two = screen.getByText("2");
+    const three = screen.getByText("3");
+    const five = screen.getByText("5");
+    const divide = screen.getByText("÷");
+    const mul = screen.getByText("×");
+    const minus = screen.getByText("-");
+    fireEvent.click(three);
+    fireEvent.click(mul);
+    fireEvent.click(two);
+    fireEvent.click(minus);
+    fireEvent.click(one);
+    fireEvent.click(divide);
+    fireEvent.click(five);
+
+    const result = await screen.findByPlaceholderText("calculate");
+    // @ts-ignore
+    expect(result.value).toBe("3×2-1÷5");
   });
 })
